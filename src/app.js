4c7d1d7cccb3eadcf709b29e0d2fd26673dd6be1
osvaldocarreramorales <osvaldocmcm@gmail.com>
2026-02-23 20:58:42 -0600

    security: implementar cors permitiendo solo peticiones desde el puerto del forntend.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const apiRoutes = require("./routes/api");
 
 const app = express();
 
+// Solo se permiten peticiones para el puerto del frontend
+const corsOptions = {
+  origin: "http://localhost:5173",
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Middleware global para mostrar logs en cada peticion
